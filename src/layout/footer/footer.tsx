5e1678cbbd7b4ab26933c8341b8fa1682116f2ec
talismanchik <talismanchik.by@gmail.com>
2023-11-23 18:41:21 +0300

    create code

--- src/layout/footer/footer.tsx
+++ src/layout/footer/footer.tsx
@@ -3,7 +3,9 @@ import s from './footer.module.scss'
 export const Footer = () => {
   return (
     <footer className={s.footerContainer}>
-      Designed and Coded by Nesterenko Eugene with <span>Love</span> & <span>Coffee</span>
+      <div>
+        Designed and Coded by Nesterenko Eugene with <span>Love</span> & <span>Coffee</span>
+      </div>
     </footer>
   )
 }
