08c7dfe30e6175a3d794cc75b75bf69a7ce3208e
talismanchik <talismanchik.by@gmail.com>
2023-11-25 11:03:40 +0300

    correct main

--- src/layout/sections/main/Home.tsx
+++ src/layout/sections/main/Home.tsx
@@ -4,22 +4,19 @@ import avatar from '../../../assets/images/avatar.jpg'
 export const Home = () => {
   return (
     <section className={s.homeContainer}>
-      <div className={s.myInform}>
-        <div className={s.textWrapper}>
-          <h1>
-            Hi there,
-            <br />
-            My name is
-            <br />
-            <span>Eugene Nesterenko</span>
-            <br />I build things for web
-          </h1>
-        </div>
-        <div className={s.avatarWrapper}>
-          <img alt={'photo of a handsome developer'} className={s.avatar} src={avatar} />
-        </div>
+      <div className={s.textWrapper}>
+        <h1>
+          Hi there,
+          <br />
+          My name is
+          <br />
+          <span>Eugene Nesterenko</span>
+          <br />I build things for web.
+        </h1>
       </div>
-      <div></div>
+      <div className={s.avatarWrapper}>
+        <img alt={'photo of a handsome developer'} className={s.avatar} src={avatar} />
+      </div>
     </section>
   )
 }
