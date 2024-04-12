8aa7bc3c19c632a4f39a0c8e04f30a1822b77651
talismanchik <talismanchik.by@gmail.com>
2024-04-12 17:05:42 +0300

    correct projects

--- src/layout/sections/main/Home.tsx
+++ src/layout/sections/main/Home.tsx
@@ -2,23 +2,29 @@ import Tilt from 'react-parallax-tilt'
 
 import s from './Home.module.scss'
 
+import cv from '../../../assets/cv.pdf'
 import avatar from '../../../assets/images/avatar.jpg'
 export const Home = () => {
   return (
     <section className={s.homeContainer} id={'home'}>
-      <div className={s.textWrapper}>
-        <h1>
-          Hi there,
-          <br />
-          My name is
-          <br />
-          <span>Eugene Nesterenko</span>
-          <br />I build things for web.
-        </h1>
+      <div className={s.homeHeader}>
+        <div className={s.textWrapper}>
+          <h1>
+            Hi there,
+            <br />
+            My name is
+            <br />
+            <span>Eugene Nesterenko</span>
+            <br />I build things for web.
+          </h1>
+        </div>
+        <Tilt className={s.avatarWrapper}>
+          <img alt={'photo of a handsome developer'} className={s.avatar} src={avatar} />
+        </Tilt>
       </div>
-      <Tilt className={s.avatarWrapper}>
-        <img alt={'photo of a handsome developer'} className={s.avatar} src={avatar} />
-      </Tilt>
+      <a href={cv} rel={'noreferrer'} target={'_blank'}>
+        <button className={s.cvButton}>download CV</button>
+      </a>
     </section>
   )
 }
